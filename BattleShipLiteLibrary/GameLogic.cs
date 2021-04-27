07288edd02ba019a27b4d1cad56742b7f316b20a
Sean <miniod2018@gmail.com>
2021-04-26 20:12:42 -0400

    Removed unused using statements

--- BattleShipLiteLibrary/GameLogic.cs
+++ BattleShipLiteLibrary/GameLogic.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Net.Sockets;
-using System.Threading;
-using System.Xml;
 using BattleShipLiteLibrary.Models;
 
 namespace BattleShipLiteLibrary
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Threading;
 using BattleShipLiteLibrary;
 using BattleShipLiteLibrary.Models;
 
